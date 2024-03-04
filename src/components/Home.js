9e00bb6e2a30eced24bc670ab1fc94ff5fc6bc8f
SK Imtiaj Uddin <imtiaj.kol@gmail.com>
2024-03-04 16:13:34 +0530

    Added Responsiveness in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ import resume from '../cResume.pdf'
 const Home = () => {
     return (
         <div className="container-fluid d-flex mt-5" id='home' style={{ backgroundColor: "#0e1630", height: "100vh" }}>
-            <div className="container home d-flex flex-column w-75" style={{ height: "75vh" }}>
+            <div className="container home-container d-flex" style={{ height: "75vh", width: "75%" }}>
                 
-                <div className="row align-items-center justify-content-center" style={{paddingTop: "20vh"}}>
-                    {/* <div className="col-1"></div> */}
+                <div className="row home" style={{paddingTop: "20vh"}}>
+                    
                     <div className="col-7">
                         <h3>Hello</h3>
                         <h3>I'm SK Imtiaj Uddin</h3>
@@ -20,8 +20,10 @@ const Home = () => {
                         <div className="btn btn-secondary" style={{backgroundColor: "rgb(220, 13, 13)"}}><a href={resume} target="_blank" rel="noreferrer" style={{color: 'white'}}>View Resume</a></div>
 
                     </div>
-                    {/* <div className="col-1"></div> */}
-                    <img src={profile} alt="profile" className="col-5 rounded-circle" style={{ height: "430px", objectFit: "cover" }} />
+
+                    <div className="col-5 rounded-circle">
+                        <img className="rounded-circle" src={profile} alt="profile" style={{ width: "100%", objectFit: "cover" }} />
+                    </div>
                 </div>
 
             </div>
